d56f86a7b45b7c82dd5999bc8103cc29641b88dd
Rahul Agarwal <57580888+agarwalrahul-22@users.noreply.github.com>
2022-10-08 12:48:43 +0530

    created CF_Template.cpp

---
+++ CF_Template.cpp
@@ -0,0 +1,27 @@
+#include     <bits/stdc++.h>
+using namespace std;
+
+// def
+#define      boost          ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define      test           int tt; cin>>tt; while(tt-->0)
+#define      pb             push_back
+#define      pp             pop_back
+#define      int            long long
+#define      mx             1000000007
+#define      fo(i,n)        for(int i=0;i<n;i++)
+#define      pb             push_back
+#define      vec            vector<int>
+// def_end
+
+
+void solve(){
+   
+}
+
+signed main(){
+    boost
+   
+    test{
+     solve();
+    }
+}
